2e48e687f29a3bf3ee0623dafe07050b18dafc54
switch86 <101955285+switch86@users.noreply.github.com>
2022-10-05 18:19:43 -0700

    Create Trivia.js
    starting a collection of trivia objects

---
+++ RPG/Trivia.js
@@ -0,0 +1,16 @@
+ { 
+   { 
+     fact: "Trans people are twice as likely to be unemployed compared with the overall population. Looking at only black trans people, that rate doubles again.", 
+     source: {
+       org: "UCLA School of Law - Williams Institute",
+       url: "https://williamsinstitute.law.ucla.edu/press/ncvs-trans-press-release/"
+     },  
+  },
+   {
+     fact: "Trans people are four times more likely than cisgender people to experience violent physical or sexual assault",
+     source: {
+        org: "UCLA School of Law - Williams Institute",
+        url: "https://williamsinstitute.law.ucla.edu/press/ncvs-trans-press-release/" 
+     },
+   }
+ }
