07768f0cf24ec7afcd07ca861930cba9ed820199
switch86 <101955285+switch86@users.noreply.github.com>
2022-10-05 18:11:51 -0700

    Update and rename Friends.jsx to Compliments.jsx
    adapt to array of objects

---
+++ RPG/Compliments.jsx
@@ -0,0 +1,27 @@
+{ 
+  id: 1,
+  person: "A stranger",
+  comment: "tells you they like your shoes.",
+  triggerWarning: false,
+}
+{
+  id: 2,
+  person: "Your parent",
+  comment: "uses your name and pronouns and affirms your gender.",
+  trigger: "Parental Death",
+  triggerWarning: true,
+}
+{
+  id: 3, 
+  person: "Your crush"
+  comment: "asks you on a cute date."
+  triggerWarning: false, 
+}
+{
+  id: 4,
+  person: "Your friend"
+  comment: "asks you to their super fun birthday plans."
+  triggerWarning: false,
+}
+
+ You end up at a friends birthday party and have a great time", "A colleague nominates you for a prestigious award", "Someone you look up to compliments your work", "You try on a new outfit and feel major gender euphoria", "You go the whole day and nobody misgenders you", "A friend calls to invite you to a fun event", "You have a really peaceful walk in the park", "You try a new activity that always felt off-limits before"]
